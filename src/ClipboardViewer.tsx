3862c9a022525839deec056222bc016a593f6ea0
Harsh Manwani <invokerharsh@gmail.com>
2024-09-09 21:32:07 +0530

    use react icons

--- src/ClipboardViewer.tsx
+++ src/ClipboardViewer.tsx
@@ -4,6 +4,8 @@ import { listen } from '@tauri-apps/api/event';
 import './ClipboardViewer.css';
 import TextItem from './Components/TextCard';
 import { ClipboardEntry } from './types';
+import { FaTrash } from 'react-icons/fa';
+
 
 function ClipboardViewer() {
   const [history, setHistory] = useState<ClipboardEntry[]>([]);
@@ -47,9 +49,7 @@ function ClipboardViewer() {
       <div className="header">
         <h2>Clipboard History</h2>
         <button onClick={clearClipboardHistory} className="clear-button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
-            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
-          </svg>
+          <FaTrash />
         </button>
       </div>
       <div className="clipboard-list">
