317cf01b745986c88a19d9e33ad440a907f41a10
Harsh Manwani <invokerharsh@gmail.com>
2024-09-11 00:55:34 +0530

    add quit option on left click

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,4 +1,5 @@
 use tauri::{
+    menu::{Menu, MenuItem},
     image::Image,
     tray::{MouseButtonState, TrayIcon, TrayIconBuilder, TrayIconEvent},
     AppHandle,
@@ -10,10 +11,21 @@ use crate::fns::emit_panel_open;
 
 pub fn create(app_handle: &AppHandle) -> tauri::Result<TrayIcon> {
     let icon = Image::from_bytes(include_bytes!("../icons/tray.png"))?;
+    let quit_i = MenuItem::with_id(app_handle, "quit", "Quit", true, None::<&str>)?;
+    let menu = Menu::with_items(app_handle, &[&quit_i])?;
 
     TrayIconBuilder::with_id("tray")
         .icon(icon)
         .icon_as_template(true)
+        .menu(&menu)
+        .menu_on_left_click(false)
+        .on_menu_event(move |app, event| match event.id.as_ref() {
+            "quit" => {
+                app.exit(0);
+            }
+            // Add more events here
+            _ => {}
+        })
         .on_tray_icon_event(|tray, event| {
             let app_handle = tray.app_handle();
 
